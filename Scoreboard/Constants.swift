73eebc3c7223a803bc9142191f9662c9d37a2541
T.J. Stone <tjstone@tjs-macbook-pro.lan>
2016-11-18 16:20:57 -0800

    Added pusher

---
+++ Scoreboard/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Scoreboard
+//
+//  Created by T.J. Stone on 11/18/16.
+//  Copyright © 2016 T.J. Stone. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let PUSHER_APP_ID: String = "84206"
+    static let PUSHER_KEY: String = "420caa3cce45adbb6930"
+    static let PUSHER_SECRET: String = "44724166baa25e29583f"
+}
---
+++ Scoreboard/PusherManager.swift
@@ -0,0 +1,14 @@
+//
+//  PusherManager.swift
+//  Scoreboard
+//
+//  Created by T.J. Stone on 11/18/16.
+//  Copyright © 2016 T.J. Stone. All rights reserved.
+//
+
+import Foundation
+
+class PusherManager {
+    
+    
+}
